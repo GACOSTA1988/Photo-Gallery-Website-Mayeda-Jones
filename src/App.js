c2edf633ce5f0ed2635d4cd6c39991c61fef84ed
George Acosta <georgeacostar@gmail.com>
2020-03-03 19:25:30 -0800

    add intial components and product dir for shopping cart test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { v4 } from "uuid";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return <div className="App"></div>;
---
+++ src/api/api.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: 1,
+    title: "Dessert",
+    price: 50,
+    category: "Lifestyle"
+  },
+  {
+    id: 2,
+    title: "Mountains",
+    price: 25,
+    category: "Adventure"
+  },
+  {
+    id: 3,
+    title: "Waterfall",
+    price: 30,
+    category: "Landscape"
+  }
+];
