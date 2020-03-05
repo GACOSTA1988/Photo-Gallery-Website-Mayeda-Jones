250da3f842640fd2c8409f02251eb85576a644fe
George Acosta <georgeacostar@gmail.com>
2020-03-05 14:45:58 -0800

    upload img assests

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Product from "./components/Product/Product";
 import Cart from "./components/Cart/Cart";
 import CheckoutForm from "./components/CheckoutForm/CheckoutForm";
 import logo from "./Logo.png";
+import stars from "./stars.png";
 import "./App.css";
 
 export default function App() {
@@ -32,33 +33,37 @@ export default function App() {
     (acc, item) => acc + item.price * item.quantity,
     0
   );
-
+  const content = {
+    backgroundImage: "url(${stars})"
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-header-text">Josh Mayeda Jones Photography</h1>
       </header>
-      <main className="App-shop">
-        <div className="App-products">
-          {items.map(item => (
-            <Product
-              key={item.title}
-              title={item.title}
-              price={item.price}
-              onAddToCartClick={() => handleAddToCartClick(item.id)}
-            />
-          ))}
-        </div>
-        <Cart itemsInCart={itemsInCart} totalCost={totalCost} />
-        {itemsInCart.length > 0 && (
-          <StripeProvider apiKey={process.env.REACT_APP_STRIPE_PUBLIC_KEY}>
-            <Elements>
-              <CheckoutForm totalCost={totalCost} />
-            </Elements>
-          </StripeProvider>
-        )}
-      </main>
+      <div style={content}>
+        <main className="App-shop">
+          <div className="App-products">
+            {items.map(item => (
+              <Product
+                key={item.title}
+                title={item.title}
+                price={item.price}
+                onAddToCartClick={() => handleAddToCartClick(item.id)}
+              />
+            ))}
+          </div>
+          <Cart itemsInCart={itemsInCart} totalCost={totalCost} />
+          {itemsInCart.length > 0 && (
+            <StripeProvider apiKey={process.env.REACT_APP_STRIPE_PUBLIC_KEY}>
+              <Elements>
+                <CheckoutForm totalCost={totalCost} />
+              </Elements>
+            </StripeProvider>
+          )}
+        </main>
+      </div>
     </div>
   );
 }
