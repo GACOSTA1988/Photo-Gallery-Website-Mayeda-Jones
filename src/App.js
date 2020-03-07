e318075e1639ce9db9c3ecfea1abee4c1f486202
George Acosta <georgeacostar@gmail.com>
2020-03-07 14:16:46 -0800

    update shooping cart components and combine them in cartcontainer

--- src/App.js
+++ src/App.js
@@ -1,69 +1,12 @@
 import React, { useState } from "react";
-import { Elements, StripeProvider } from "react-stripe-elements";
-import items from "./api/items";
-import Product from "./components/Product/Product";
-import Cart from "./components/Cart/Cart";
-import CheckoutForm from "./components/CheckoutForm/CheckoutForm";
-import logo from "./Logo.png";
-import stars from "./stars.png";
-import "./App.css";
+import CartContainer from "./components/CartContainer/CartContainer";
 
-export default function App() {
-  const [itemsInCart, setItemsInCart] = useState([]);
-
-  const handleAddToCartClick = id => {
-    setItemsInCart(itemsInCart => {
-      const itemInCart = itemsInCart.find(item => item.id === id);
-
-      // if item is already in cart, update the quantity
-      if (itemInCart) {
-        return itemsInCart.map(item => {
-          if (item.id !== id) return item;
-          return { ...itemInCart, quantity: item.quantity + 1 };
-        });
-      }
-
-      // otherwise, add new item to cart
-      const item = items.find(item => item.id === id);
-      return [...itemsInCart, { ...item, quantity: 1 }];
-    });
-  };
-
-  const totalCost = itemsInCart.reduce(
-    (acc, item) => acc + item.price * item.quantity,
-    0
-  );
-  const content = {
-    backgroundImage: "url(${stars})"
-  };
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-header-text">Josh Mayeda Jones Photography</h1>
-      </header>
-      <div style={content}>
-        <main className="App-shop">
-          <div className="App-products">
-            {items.map(item => (
-              <Product
-                key={item.title}
-                title={item.title}
-                price={item.price}
-                onAddToCartClick={() => handleAddToCartClick(item.id)}
-              />
-            ))}
-          </div>
-          <Cart itemsInCart={itemsInCart} totalCost={totalCost} />
-          {itemsInCart.length > 0 && (
-            <StripeProvider apiKey={process.env.REACT_APP_STRIPE_PUBLIC_KEY}>
-              <Elements>
-                <CheckoutForm totalCost={totalCost} />
-              </Elements>
-            </StripeProvider>
-          )}
-        </main>
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <CartContainer />
       </div>
-    </div>
-  );
+    );
+  }
 }
