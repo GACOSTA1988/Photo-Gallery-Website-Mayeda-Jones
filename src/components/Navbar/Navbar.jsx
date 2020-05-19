8729ae2851793630bdfe5292782301c01590815a
George Acosta <georgeacostar@gmail.com>
2020-05-19 01:05:48 -0700

    add gallery navbar anchor styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import GoogleFontLoader from "react-google-font-loader";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
@@ -8,18 +7,52 @@ import Container from "react-bootstrap/Container";
 function NavBar(props) {
   return (
     <Container fluid>
-      <div className="NavbarMain">
-        <Navbar collapseOnSelect expand="lg" bg="black" variant="dark">
-          <Navbar.Brand href="#/">HOME</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#/mainGallery">GALLERY</Nav.Link>
-              <Nav.Link href="#/about">ABOUT</Nav.Link>
-              <Nav.Link href="#/shoppingCart">SHOP</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+      <div className="NavbarMainContainer">
+        <br />
+        <a
+          href="#/"
+          style={{
+            fontWeight: "bolder",
+            fontFamily: "BowlbyOneSC",
+            fontSize: "2vw",
+            marginBottom: "5px",
+          }}
+        >
+          HOME
+        </a>
+        <a
+          href="#/mainGallery"
+          style={{
+            fontWeight: "bolder",
+            fontFamily: "BowlbyOneSC",
+            fontSize: "2vw",
+            marginLeft: "5%",
+          }}
+        >
+          GALLERY
+        </a>
+        <a
+          href="#/about"
+          style={{
+            fontWeight: "bolder",
+            fontFamily: "BowlbyOneSC",
+            fontSize: "2vw",
+            marginLeft: "5%",
+          }}
+        >
+          ABOUT
+        </a>
+        <a
+          href="#/shoppingCart"
+          style={{
+            fontWeight: "bolder",
+            fontFamily: "BowlbyOneSC",
+            fontSize: "2vw",
+            marginLeft: "5%",
+          }}
+        >
+          SHOP
+        </a>
       </div>
     </Container>
   );
